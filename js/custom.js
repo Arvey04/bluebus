abd38330822be6f2d310c39e6529bcf75b1d7266
unknown <vrohit563@gmail.com>
2021-05-28 16:22:56 +0530

    first commit

---
+++ js/custom.js
@@ -0,0 +1,29 @@
+(function($) {
+	
+	/* Sticky Header */
+    // $(window).scroll(function() {
+    //     if ($(this).scrollTop() > 40) {
+    //         $('.header').addClass("sticky");
+    //     } else {
+    //         $('.header').removeClass("sticky");
+    //     }
+    // });
+	
+	/* Go up */
+    // jQuery(window).scroll(function() {
+    //     if (jQuery(this).scrollTop() > 800) {
+    //         jQuery(".go-up").css("bottom", "10px");
+    //     } else {
+    //         jQuery(".go-up").css("bottom", "-60px");
+    //     }
+    // });
+    // jQuery(".go-up").click(function() {
+    //     jQuery("html,body").animate({
+    //         scrollTop: 0
+    //     }, 1500);
+    //     return false;
+    // });
+    
+
+    
+})(jQuery);
